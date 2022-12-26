a89f710bcbddcf080dfb51e319d69c712cca95d9
Angelo Kusuma <84068505+Akusuma05@users.noreply.github.com>
2022-12-26 22:23:00 +0700

    Ngerapihin Info Documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,15 +9,8 @@ use Illuminate\Routing\Controller as BaseController;
 /**
      * @OA\Info(
      *      version="1.0.0",
-     *      title="Integration Swagger in Laravel with Passport Auth Documentation",
-     *      description="Implementation of Swagger with in Laravel",
-     *      @OA\Contact(
-     *          email="admin@admin.com"
-     *      ),
-     *      @OA\License(
-     *          name="Apache 2.0",
-     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
-     *      )
+     *      title="Documentation for Agent Valo API",
+     *      description="API for all valorant agents",
      * )
      *
      * @OA\Server(
